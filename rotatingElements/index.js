6823c9bbd54e226a6a26dd51b56300e67c2c1324
jerobejrobinson <jerobejrobinson@gmail.com>
2022-09-22 14:45:30 -0500

    rotating elements using linked list

--- rotatingElements/index.js
+++ rotatingElements/index.js
@@ -1,7 +1,28 @@
 class Node {
     constructor(data) {
         this.data = data;
-        this.next = null
+        this.next = null;
+        this.prev = null;
+    }
+
+    isActive(selector) {
+        if(this.data.classList.contains(selector)) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    removeClass(selector) {
+        if(this.isActive(selector)) {
+            this.data.classList.remove(selector)
+        }
+    }
+
+    addClass(selector) {
+        if(!this.isActive(selector)) {
+            this.data.classList.add(selector)
+        }
     }
 }
 
@@ -13,11 +34,21 @@ function convertArrToCirLinkedList(arr) {
         const nIndex = index + 1
         if(item.next == null && nIndex < arr.length) {
             item.next = arr[nIndex]
-            return item
         } else {
-            return item.next = arr[0]
+            item.next = arr[0]
         }
+        return item
     })
+    .map((item, index, arr) => {
+        let pIndex = index - 1
+        if(item.prev == null && index == 0) {
+            item.prev = arr[arr.length - 1]
+        } else {
+            item.prev = arr[pIndex]
+        }
+        return item
+    })
+
     return linkedList[0]
 }
 
@@ -28,14 +59,22 @@ function getElementsByClass(selector) {
 
 const Head = convertArrToCirLinkedList(getElementsByClass('.rotate'))
 
-function slider(head) {
-    head.data.classList.add('active')
-    setTimeout(() => {
-        if(head.next != null) {
-            head.data.classList.remove('active')
-            slider(head.next)
-        }
-    },  5000)
+let currentNode = Head;
+
+function handlePrev(node) {
+    node.removeClass('active')
+    currentNode = node.prev
+    currentNode.addClass('active')
 }
 
-slider(Head)
+function handleNext(node) {
+    node.removeClass('active')
+    currentNode = node.next
+    currentNode.addClass('active')
+}
+
+let prevBtn = document.querySelector('#Prev')
+let nextBtn = document.querySelector('#Next')
+
+prevBtn.addEventListener('click', () => handlePrev(currentNode))
+nextBtn.addEventListener('click', () => handleNext(currentNode))
