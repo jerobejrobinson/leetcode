49045a364756f6fca343fdf5390277836547e056
jerobe <jerobejrobinson@gmail.com>
2022-09-22 07:06:40 -0500

    completed two sums

---
+++ two-sums/index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    let solution = false;
+    let map = new Map();
+    
+    nums.every((num, index) => {
+        let rn = target - num;
+        if(map.get(rn) == undefined) {
+            map.set(num, index)
+            return true
+        } else {
+            solution = [map.get(rn), index]
+        }
+    })
+    
+    return solution
+};
