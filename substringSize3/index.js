3dd928b064c21998ae795de4480813f421c624e1
jerobejrobinson <jerobejrobinson@gmail.com>
2022-09-26 13:37:02 -0500

    learning sliding window

--- substringSize3/index.js
+++ substringSize3/index.js
@@ -1,18 +1,16 @@
 // sliding window
 function substringSize(str) {
     let count = 0;
-    let [left, middle, right] = [0, 1, 2];
+    let [left, right] = [0, 2];
     
     while(right < str.length) {
-        console.log()
-        if(str[middle] == str[left] || str[middle] == str[right] || str[left] == str[right]) {
+        if(str[left + 1] == str[left] || str[left + 1] == str[right] || str[left] == str[right]) {
             
-        } else {
-            count++
-        }
+        } else count++
         left++;
-        middle++;
         right++;
     }
     return count
 }
+
+console.log(substringSize("asc"))
