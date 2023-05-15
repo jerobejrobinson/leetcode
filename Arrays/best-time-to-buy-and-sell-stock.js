689d862d8be7c04e112a53024ed5ade2ddd5354a
jerobejrobinson <jerobejrobinson@gmail.com>
2023-05-15 12:39:29 -0500

    completed best time to buy stocks

--- Arrays/best-time-to-buy-and-sell-stock.js
+++ Arrays/best-time-to-buy-and-sell-stock.js
@@ -6,24 +6,28 @@
     Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
  */
 
-const prices = [7,6,4,3,1];
+const prices = [3,2,6,5,0,3];
 
 const maxProfit = (prices) => {
     let [left, right, lowestIndex, highestIndex] = [ 0, 1, 0, 1]
 
+    let maxProfit = 0;
+    
     if(prices.length <= 1) return 0
 
     while(right < prices.length) {
-        lowestIndex = (prices[left] < prices[lowestIndex] ? left : lowestIndex)
-        highestIndex = (prices[right] > prices[highestIndex] ? right : highestIndex)
+        lowestIndex = (prices[left] <= prices[lowestIndex] ? left : lowestIndex)
+        highestIndex = ((prices[right] > prices[highestIndex]) ? right : (highestIndex < left) ? right : highestIndex)
+
+        let profit = (prices[highestIndex] - prices[lowestIndex])
+
+        maxProfit = Math.max(profit, maxProfit)
 
         left = right
         right++
     }
 
-    if(highestIndex > lowestIndex) return ((prices[highestIndex] - prices[lowestIndex]) < 0 ? 0 : (prices[highestIndex] - prices[lowestIndex]))
-
-    return 0
+    return maxProfit
 }
 
 console.log(maxProfit(prices))
